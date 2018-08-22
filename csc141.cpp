bfb73b29a7d2e419df35f15dbd600c64d73cabde
vna02 <42207441+vna02@users.noreply.github.com>
2018-08-22 15:23:02 +0530

    Add files via upload

---
+++ csc141.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a,b;
+cout<<"enter the base and height of triangle";
+cin>>a>>b;
+cout<<"\n the area of the triangle is"<<0.5*a*b;
+// your code goes here
+return 0;
+}
---
+++ veena.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a,b;
+cout<<"\n the third angle is "<< 180-a-b;
+// your code goes here
+return 0;
+}
