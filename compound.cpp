b5d82499a4ae955a7c730fb9f506e8ef7eafef84
vna02 <42207441+vna02@users.noreply.github.com>
2018-08-22 16:02:27 +0530

    Add files via upload

---
+++ compound.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+float p,r,i;
+int n;
+cout<<"enter the principal amount, rate of interest and number of years";
+cin>>p>>r>>n;
+cout<<"the interest is"<<p*pow(i,n)-p;
+// your code goes here
+return 0;
+}
