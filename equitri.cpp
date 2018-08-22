84d0ebdb55496c68e607554021f3d86d27bab607
vna02 <42207441+vna02@users.noreply.github.com>
2018-08-22 15:55:37 +0530

    Add files via upload

---
+++ equitri.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a;
+cout<<"enter the side of triangle";
+cin>>a;
+cout<<"\n the area of the triangle is "<<(sqrt(3)/4)*a*a;
+// your code goes here
+return 0;
+}
